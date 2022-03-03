e39144bd919cee0ef28cce6f438313ce0cad3168
Nguyen Loc <68978514+baloc2000ct@users.noreply.github.com>
2022-03-03 10:12:45 +0700

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,5 +1,4 @@
-
- let arr = [0 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr = [0 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function binarySearch(){
 
@@ -11,19 +10,18 @@ function binarySearch(){
            }
        }
        return -1;*/
-
+       let outputText;
        let lower = 0;
        let upper = arr.length -1;
-       let val = document.forms["input_form3"]["val"].value;
+       let val =  parseInt(document.forms["input_form3"]["val"].value);
 
      
        
     while(lower <= upper){
 
-        let middle = lower + Math.floor((upper + lower)/2);
-
+        let middle = Math.floor((upper + lower)/2);
         if(val === arr[middle]){
-             outputText ="Present" + middle;
+            outputText ="Present number " + val + " at position " + middle + " in array";
             break;
         }
         else if(val > arr[middle]){
@@ -32,15 +30,10 @@ function binarySearch(){
         }else
         {
             upper =middle - 1;    
-        }
-         outputText =" Can find any number like this in array!"
+        }       
+        outputText =" Can't find any number like this in array!";
     }
-
      document.getElementById("output_text3").innerHTML = outputText;
      
      
 }
-
-binarySearch();
-
-
